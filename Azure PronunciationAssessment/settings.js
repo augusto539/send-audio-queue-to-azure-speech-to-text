1f64d5346be70bd65fd7d32f1e6c8c5349e21e76
augusto <augustoantonelli88@gmail.com>
2023-05-26 15:36:54 -0300

    delet keys

--- Azure PronunciationAssessment/settings.js
+++ Azure PronunciationAssessment/settings.js
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+require('dotenv').config();
+
 (function() {
     "use strict";
       
@@ -8,13 +10,13 @@
       
         // Replace with your own subscription key, service region (e.g., "westus"),
         // and recognition language.
-        subscriptionKey:   "e25c6a319ed3461fb3baf42825d347fb",
-        serviceRegion:     "eastus", // e.g., "westus"
-        language:          "es-es",
+        subscriptionKey:   process.env.subscriptionKey,
+        serviceRegion:     process.env.serviceRegion, // e.g., "westus"
+        language:          process.env.language,
       
         // Replace with the full path to a wav file you want to recognize or overwrite.
         // filename:          "audio pivito.wav", // 16000 Hz, Mono
-        filename:          "AudioPivito.wav", // 16000 Hz, Mono
+        filename:          "your audio.wav", // 16000 Hz, Mono
       
         // Replace with your own Language Understanding subscription key (endpoint
         // key), region, and app ID in case you want to run the intent sample.
